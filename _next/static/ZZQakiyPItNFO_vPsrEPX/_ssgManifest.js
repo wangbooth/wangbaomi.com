50edf6794598c1e373c81a97ddcd38d31dcccde6
wangbooth <wangbooth@gmail.com>
2026-07-16 17:25:19 +0800

    Deploy wangbaomi.com

---
+++ _next/static/ZZQakiyPItNFO_vPsrEPX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002F[slug]","\u002Fblogs\u002Fcategories\u002F[category]","\u002Fblogs\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
